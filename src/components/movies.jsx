2f0979a60eb4198b65a1b6ffbbd9a879244fd427
Sicelo_Mduna <sicelo.mduna@globalmicro.co.za>
2024-10-04 11:29:33 +0200

    Filtering - Implimenting Filtering

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -42,15 +42,21 @@ class Movies extends Component {
 
   handleGenreSelect = (genre) => {
 
+    this.setState({ selectedGenre: genre});
+
 
   };
 
   render() {
     const { length: count } = this.state.movies;
-    const { pageSize, currentPage, movies: allMovies } = this.state;
+    const { pageSize, currentPage, selectedGenre, movies: allMovies } = this.state;
     if (count === 0) return <p>No movies in the database.</p>;
 
-    const movies = paginate(allMovies, currentPage, pageSize);
+    const filtered = selectedGenre
+      ? allMovies.filter((m) => m.genre._id === selectedGenre._id)
+      : allMovies;
+
+    const movies = paginate(filtered, currentPage, pageSize);
 
     return (
       <>
@@ -58,13 +64,12 @@ class Movies extends Component {
           <div className="col-3">
             <ListGroup
               items={this.state.genres}
-              textProperty = "name"
-              valueProperty = "_id"
+              selectedItem={this.state.selectedGenre}
               onItemSelect={this.handleGenreSelect}
             />
           </div>
           <div className="col">
-            <p>Showing {count} movies in the database.</p>
+            <p>Showing {filtered.length} movies in the database.</p>
             <table class="table">
               <thead>
                 <tr>
@@ -102,7 +107,7 @@ class Movies extends Component {
               </tbody>
             </table>
             <Pagination
-              itemsCount={count}
+              itemsCount={filtered.length}
               pageSize={pageSize}
               currentPage={currentPage}
               onPageChange={this.handlePageChange}
