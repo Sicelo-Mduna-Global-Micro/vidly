00ec5a423bca4475c7f51c83d74e24a5d6963212
Sicelo_Mduna <sicelo.mduna@globalmicro.co.za>
2024-10-06 21:06:02 +0200

    Sorting - Unique Keys Final

--- src/components/common/tableBody.jsx
+++ src/components/common/tableBody.jsx
@@ -10,7 +10,10 @@ class TableBody extends Component {
         else{
                 return _.get(item, column.path);
         }
-    
+    }
+
+    createKey = (item, column) => {
+        return item._id + (column.path || column.key);
     }
     
     render() {
@@ -19,9 +22,9 @@ class TableBody extends Component {
           <>
             <tbody>
               {data.map((item) => (
-                <tr>
+                <tr key = {item._id}>
                   {columns.map((column) => (
-                    <td>
+                    <td key = {this.createKey}>
                         {this.renderCell(item, column)}
                     </td>
                   ))}
