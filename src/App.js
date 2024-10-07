9dccf06a704f009ce8014110900164138442eb10
Sicelo_Mduna <sicelo.mduna@globalmicro.co.za>
2024-10-07 21:05:43 +0200

    Adding Routing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import './App.css';
-import Movies from './components/movies';
-import Like from './components/common/like';
-import Pagination from './components/pagination';
-
-function App() {
-  return (
-    <>
-      <main className = "container">
-        <Movies/>
-        {/* <Like/> */}
-        {/* <Pagination/> */}
+import React, { Component } from 'react'
+import { Route, Redirect, Switch } from "react-router-dom";
+import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import "./App.css";
 
 
+class App extends Component {
+  render() { 
+    return (
+      <main className="container">
+        <Switch>
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
       </main>
-    </>
-  );
+    );
+  }
 }
-
+ 
 export default App;
